95c1f77b1466ae4a075aac3c7186763509bb2c85
sharonshiri <sharon>
2019-01-14 04:10:49 +0200

    .

--- FileCacheManager.cpp
+++ FileCacheManager.cpp
@@ -7,20 +7,22 @@
 FileCacheManager::FileCacheManager() {
     loadToMap();
 }
+
 void FileCacheManager::loadToMap() {
     string line = "";
     fstream file;
-    file.open(solutionsFile, ios::in);
+    file.open(solutionsFile);
     if (file.is_open()) {
         // while there are more lines
         while (getline(file, line)) {
             // take care of one line from the text
             parseFile(line);
         }
-        file.close();
     } else {
-        file.open(solutionsFile, ios::trunc);
+        file.open(solutionsFile, ios::out);
     }
+    file.flush();
+    file.close();
 }
 
 void FileCacheManager::parseFile(string line) {
@@ -47,11 +49,11 @@ void FileCacheManager::saveInCache(string problem, string solution) {
     myMap.insert(make_pair(problem,solution));
     //save to file
     fstream file;
-    file.open(solutionsFile, ios::app | ios::trunc);
-    if (file.is_open()) {
-        file << problem << "\n" << "$\n" << solution << "\n$$" << endl;
-    } else {
-        file.open(solutionsFile, ios::trunc);
+    file.open(solutionsFile, ios::app);
+    if (!(file.is_open())) {
+        file.open(solutionsFile, ios::trunc | ios::out);
     }
+    file << problem << "\n" << "$\n" << solution << "\n$$" << endl;
+    file.flush();
     file.close();
 }
